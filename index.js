1df1a3d526cb68d6de75dc809954856099f9b1f0
Ilma Makarevic <makarevic85@gmail.com>
2024-05-19 22:19:00 +0200

    added necklaces, rings, and bracelets

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('earrings-link').addEventListener('click', function() {
+    window.location.href = 'earrings.html';
+});
+
+document.getElementById('necklaces-link').addEventListener('click', function() {
+    window.location.href = 'necklaces.html';
+});
+
+document.getElementById('rings-link').addEventListener('click', function() {
+    window.location.href = 'rings.html';
+});
+
+document.getElementById('bracelets-link').addEventListener('click', function() {
+    window.location.href = 'bracelets.html';
+});

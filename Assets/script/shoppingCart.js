7f847f4f49bc0edaa4f9fdb91d84bf64d719cf9e
Hasan Smajlović <smajlovic.hasan02@gmail.com>
2024-05-20 23:55:39 +0200

    Checkout design added

--- Assets/script/shoppingCart.js
+++ Assets/script/shoppingCart.js
@@ -5,15 +5,3 @@ const appSettings = {
     databaseURL: "https://emporium-23763-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const itemsInDB = ref(database, "ShoppingCart/Items");
-
-const inputField = document.getElementById("inputField");
-const addButton = document.getElementById("addButton");
-
-addButton.addEventListener("click", function () {
-    let inputValue = inputField.value;
-    push(itemsInDB, inputValue);
-    console.log(`${inputValue} added to database`);
-});
